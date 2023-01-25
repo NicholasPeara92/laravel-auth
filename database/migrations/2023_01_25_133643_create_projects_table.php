015f2b0d2e9922313113855ef294daab305c07a8
NicholasPeara92 <nicholas.peara@gmail.com>
2023-01-25 15:10:35 +0100

    Add projects to the database

--- database/migrations/2023_01_25_133643_create_projects_table.php
+++ database/migrations/2023_01_25_133643_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150)->unique();
+            $table->string('company', 50);
+            $table->text('description');
+            $table->string('slug');
             $table->timestamps();
         });
     }
